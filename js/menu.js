69be77f4cbda1eb5447f752552a3b80373267575
EvoluTech <100075749+EvoluTech@users.noreply.github.com>
2022-09-18 18:48:12 +0300

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,14 @@
+// const menuHumb =document.querySelector('.menu-round')
+window.addEventListener("DOMContentLoaded", (event) => {
+
+
+    document.querySelector(".loader").style.display="none";
+
+    document.querySelector("body").style.overflow="initial"
+    
+
+    
+   
+console.log("DOM entièrement chargé et analysé");
+});
+
